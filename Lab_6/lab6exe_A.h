4b62acb9d265be61be2e32138fc3973e39951601
Matthueh <82128424+Matthueh@users.noreply.github.com>
2021-12-25 17:48:07 -0700

    Add files via upload

---
+++ Lab_6/lab6exe_A.h
@@ -0,0 +1,15 @@
+//  ENSF 337 - Fall 2020 Lab 6 Exercise A
+//  M. Moussavi
+//  lab6exe_A.h
+
+
+#ifndef lab6ExA_h
+#define lab6ExA_h
+
+typedef struct String {
+    char* text;
+    int length;
+} String;
+
+
+#endif /* lab6ExA_h */
